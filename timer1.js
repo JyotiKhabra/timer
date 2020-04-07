b480a99f8f08097b31b58001cabd97ca66aee371
vagrant <j_khabra11@hotmail.com>
2020-04-07 22:32:35 +0000

    updated to meet limitations

--- timer1.js
+++ timer1.js
@@ -1,7 +1,9 @@
 const args = process.argv;
 let commandLine = args.slice(2);
 for (const nums of commandLine) { 
+  if(Number(nums) && Number(nums)>= 0){
   setTimeout(function() {
     process.stdout.write('\x07')
   }, Number(nums)*1000);
+}
 }
